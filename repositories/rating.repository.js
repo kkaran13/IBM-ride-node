7597763dcdb23aca52cb4996b65ceb1b9a278bcc
554282003 <vivek200328vishwakarma@gmail.com>
2025-09-07 00:24:46 +0530

    minor change in databse operation function

--- repositories/rating.repository.js
+++ repositories/rating.repository.js
@@ -1,24 +1,26 @@
 
 import Rating from "../models/mongodbmodels/rating.model.js";
 
-export default class RatingRepository {
-  static async create(ratingData) {
+class RatingRepository {
+   async create(ratingData) {
     return await Rating.create(ratingData);
   }
 
-  static async findById(id) {
+   async findById(id) {
     return await Rating.findById(id);
   }
 
-  static async findByRide(ride_id) {
+   async findByRide(ride_id) {
     return await Rating.find({ ride_id });
   }
 
-  static async findByRider(rider_id) {
+   async findByRider(rider_id) {
     return await Rating.find({ rider_id });
   }
 
-  static async update(id, updateData) {
+   async update(id, updateData) {
     return await Rating.findByIdAndUpdate(id, updateData, { new: true });
   }
 }
+
+export default new RatingRepository();
