847c770c9997ced0363898ff118c4e170e307d54
554282003 <vivek200328vishwakarma@gmail.com>
2025-09-06 19:06:44 +0530

    feat(payment-controller): add payment controller
    Added handlers for create, get by id, update status, and list by rider
    Used asyncHandler for error safety and ApiResponse for consistency

---
+++ controllers/payments.contoller.js
@@ -0,0 +1,44 @@
+import PaymentService from "../services/payment.service.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+class PaymentController {
+  createPayment = asyncHandler(async (req, res) => {
+    const payment = await PaymentService.createPayment(req.body, req.user);
+    return res
+      .status(201)
+      .json(new ApiResponse(201, payment, "Payment created successfully"));
+  });
+
+  getPaymentById = asyncHandler(async (req, res) => {
+    const payment = await PaymentService.getPaymentById(req.params.id, req.user);
+    return res
+      .status(200)
+      .json(new ApiResponse(200, payment, "Payment fetched successfully"));
+  });
+
+  updatePaymentStatus = asyncHandler(async (req, res) => {
+    const {status} = req.body || {}
+    
+    const payment = await PaymentService.updatePaymentStatus(
+      req.params.id,
+      status,
+      req.user
+    );
+    return res
+      .status(200)
+      .json(new ApiResponse(200, payment, "Payment status updated successfully"));
+  });
+
+  getPaymentsByRider = asyncHandler(async (req, res) => {
+    const payments = await PaymentService.getPaymentsByRider(
+      req.params.rider_id,
+      req.user
+    );
+    return res
+      .status(200)
+      .json(new ApiResponse(200, payments, "Payments fetched successfully"));
+  });
+}
+
+export default new PaymentController();
